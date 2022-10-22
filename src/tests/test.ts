83dcaf363e521e269784126855b1815d8192eed8
LE Cong Tuan <congtuanle23@gmail.com>
2022-10-22 22:42:23 +0200

    feat: convert int to roman and test

---
+++ src/tests/test.ts
@@ -0,0 +1,12 @@
+import Romint from '..';
+import { describe, expect, test } from '@jest/globals';
+
+describe('Compare results', () => {
+  test('1-3999', () => {
+    for (let i = 1; i <= 3999; i++) {
+      const roman = Romint.parseRoman(i);
+      const num = Romint.parseInt(roman);
+      expect(num).toEqual(i);
+    }
+  });
+});
